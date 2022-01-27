be7d254c1cf8a85c087dc2223466b084a59913c6
Nikebeamz <58497036+Nikebeamz@users.noreply.github.com>
2022-01-27 10:46:17 -0500

    Update test.java

--- CCProject/src/test.java
+++ CCProject/src/test.java
@@ -1,8 +1,10 @@
-
+import java.util.Scanner*;
 public class test {
 	public static void main(String args[]) {
 		System.out.println("hello");
 		System.out.println("WORKS?!!");
 		System.out.println("GitHub has been fully integrated");
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLine());
 	}
 }
