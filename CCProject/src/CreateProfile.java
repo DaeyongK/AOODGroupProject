236ec53a53f1fe4985a2da89dfc685f03df163e4
toh8473 <terraoh8473@gmail.com>
2022-02-15 11:07:24 -0500

    CreateProfile, SelectProfile, and Options class

---
+++ CCProject/src/CreateProfile.java
@@ -0,0 +1,16 @@
+
+public class CreateProfile extends QPanel{
+	TransitionButton done;
+	EstablisherButton create;
+	
+	CreateProfile(String title){
+		//specify parameters
+		done = new TransitionButton(int width, int height, Color color, String text, int screenID, int buttonID);
+		create = new EstablisherButton(int width, int height, Color color, String text, int screenID, int buttonID);
+	}
+	
+	private radioClick() {
+		
+	}
+
+}
