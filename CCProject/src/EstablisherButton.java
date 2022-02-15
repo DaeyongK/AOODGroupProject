773550a54be547f7ef2c7d82a54c0baf2c69df49
Aben2901 <abner04ben@gmail.com>
2022-02-15 12:13:39 -0500

    buttonclasses

--- CCProject/src/EstablisherButton.java
+++ CCProject/src/EstablisherButton.java
@@ -1,4 +1,11 @@
-
-public class EstablisherButton {
+import javax.swing.*;
+import java.awt.*;
+public class EstablisherButton extends QButton{
+	EstablisherButton(int width, int height, Color color, String text, int screenID, int buttonID){
+		
+	}
+	public void mousePressed() {
+		
+	}
 
 }
--- CCProject/src/QButton.java
+++ CCProject/src/QButton.java
@@ -1,4 +1,12 @@
-
-public class QButton {
-
+import javax.swing.*;
+import java.awt.*;
+abstract class QButton extends JButton{
+	final Color BACKGROUND_COLOR= new Color(32,18,77);//purple
+	final Color TITLE_COLOR= new Color(240,193,67);//yellow
+	private int buttonID;
+	QButton(int width, int height, Color color, String text, int buttonID){
+		
+	}
+	public abstract void mousePressed();	
+	public abstract void mouseOver();
 }
