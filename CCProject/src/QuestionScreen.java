fa6015379305e5b76f92026d443cc36e4bdfd13b
kchen3490 <kchen3490@gmail.com>
2022-02-17 11:15:33 -0500

    Update QuestionScreen.java

--- CCProject/src/QuestionScreen.java
+++ CCProject/src/QuestionScreen.java
@@ -1,8 +1,25 @@
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+//QuestionScreen is front-end class made by Kai C.
 
-//Kai Chen
+import java.awt.*;
+import javax.swing.*;
+
 public class QuestionScreen extends QPanel {
+	private String title;
+	private int id = 0;
+
+	QuestionScreen(String t) {
+		super(t);
+	}
+	public int getScreenID() {
+		return id;
+	}
+	public boolean popup(String text) {
+		
+	}
+	public void buttonClicked(int buttonID) {
+		
+	}
 	public static void main(String[] arg) {
-		System.out.println("Testing");
+		new QuestionScreen("QuestionScreen Test");
 	}
 }
