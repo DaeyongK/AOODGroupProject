9fdfae62fe17bb730b5c9b44bf744e5659c01bb6
Nikebeamz <58497036+Nikebeamz@users.noreply.github.com>
2022-02-17 12:14:13 -0500

    Update ImportScreen.java

--- CCProject/src/ImportScreen.java
+++ CCProject/src/ImportScreen.java
@@ -1,25 +1,49 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 class ImportScreen extends QPanel{
-  private File xmlToBeConverted;
   private boolean filePresent = false;
+  private TransitionButton done = new TransitionButton(200, 50, Color.WHITE, "Done", 8, 0);
+  done.setVisible(false);
+  private TransitionButton back = new TransitionButton(200, 50, Color.WHITE, "Back", 8, 1);
+  private EstablisherButton chooseFile;
+  private String title;
+  private QPanel importPain = new QPanel("Import Domain");
+  private JLabel fileName;
   ImportScreen(){
-    //Add QButtons and stuff, along with a file 
-    TransitionButton done = new TransitionButton(200, 50, Color.WHITE, "Done", Quizit.IMPORTID, 0)
-    TransitionButton back = new TransitionButton(200, 50, Color.WHITE, "Done", Quizit.IMPORTID, 1);
-    EstablisherButton chooseFile;
+    importPain.setLayout(new BoxLayout(importPain, BoxLayout.Y_AXIS));
+    importPain.add(new JLabel(title));
   }
   public int getScreenID(){
-    return Quizit.IMPORTID; 
+    return 8; 
+  }
+	public boolean popup(String text){
+    return true;
   }
   public buttonClicked(int buttonID){
     switch(buttonID){
       case 0:
-        returnDomain(xmlTOBeConverted);
+        Domain d = new Domain(file);
         break;
-      case 2:
-        break;     
+      case 1:
+        break;
     }      
   }
-  private Domain returnDomain(File f){
-    return new Domain(f);
-  }
+  public boolean accept(File f) {
+    if (f.isDirectory()) {
+        return true;
+    }
+    private boolean accept(File f){
+      String extension = Utils.getExtension(f);
+      if (extension != null) {
+        if (extension.equals(Utils.xml)) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+
+      return false;
+    }
+}
 }
