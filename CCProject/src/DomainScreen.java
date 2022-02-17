d96aec1c10acf619da1b5d722e9a9938e6e56609
PinkishUnicorns <98037140+PinkishUnicorns@users.noreply.github.com>
2022-02-17 10:47:26 -0500

    Update DomainScreen.java

--- CCProject/src/DomainScreen.java
+++ CCProject/src/DomainScreen.java
@@ -1,4 +1,11 @@
 
-public class DomainScreen {
-
+public abstract class DomainScreen extends QPanel{
+	String name;
+	DomainScreen(String input){
+		name = input;
+	}
+	public String getName() {
+		return name; 
+	}
+	buttonClicked
 }
