11d669b5ba1738d06d49ffe3868ec39844d63b3d
Aben2901 <abner04ben@gmail.com>
2022-02-17 15:16:21 -0500

    Qbutton

--- CCProject/src/EstablisherButton.java
+++ CCProject/src/EstablisherButton.java
@@ -1,11 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
+//Abner Ben
 public class EstablisherButton extends QButton{
 	EstablisherButton(int width, int height, Color color, String text, int screenID, int buttonID){
+		super(width,height,color,text,buttonID);
+		JButton b=new JButton("text");  
+		b.setBounds(50,50,width,height);
+		b.setBackground(TITLE_COLOR);
 		
 	}
 	public void mousePressed() {
 		
+	}
+	public void mouseOver() {
 	}
 
 }
--- CCProject/src/QButton.java
+++ CCProject/src/QButton.java
@@ -1,11 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+//Abner Ben
 abstract class QButton extends JButton{
 	final Color BACKGROUND_COLOR= new Color(32,18,77);//purple
 	final Color TITLE_COLOR= new Color(240,193,67);//yellow
-	private int buttonID;
+	private int buttonId;
 	QButton(int width, int height, Color color, String text, int buttonID){
-		
+		JButton b=new JButton("text");  
+		b.setBounds(50,50,width,height);
+		b.setBackground(TITLE_COLOR);
+		buttonId = buttonID;
 	}
 	public abstract void mousePressed();	
 	public abstract void mouseOver();
