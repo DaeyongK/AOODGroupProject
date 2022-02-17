383093e834c0fc838a093a29f7d2caa2eb534498
kchen3490 <78284992+kchen3490@users.noreply.github.com>
2022-02-17 12:14:14 -0500

    Update QuestionScreen.java

--- CCProject/src/QuestionScreen.java
+++ CCProject/src/QuestionScreen.java
@@ -5,10 +5,21 @@ import javax.swing.*;
 
 public class QuestionScreen extends QPanel {
 	private String title;
-	private int id = 0;
+	private int id;
+	private boolean graphicDetected;
+	private JTextField questionBox;
+	private JTextField answerBox;
+	
+	private establisherBtn attachGraphic;
+	private establisherBtn detachGraphic;
+	private transitionBtn backBtn;
 
 	QuestionScreen(String t) {
-		super(t);
+		super(t);//Will this set the title variable in this class to t?
+		if (t.equals("Create a Question"))
+		    id = 12;
+		else
+		    id = 13;
 	}
 	public int getScreenID() {
 		return id;
@@ -17,7 +28,30 @@ public class QuestionScreen extends QPanel {
 		
 	}
 	public void buttonClicked(int buttonID) {
+		if(buttonID == 0) {
+			
+		}
+		else if(buttonID == 1) {
+			
+		}
+		else if (buttonID == 2) {
+			
+		}
+		else if (buttonID == 3) {
+			
+		}
+		else if (buttonID == 4) {
+			
+		}
+		else {
+			
+		}
+	}
+	private void radioClick() {
 		
+	}
+	public void main paintComponent(Graphic g) {
+		g.drawRectangle(1280,720,
 	}
 	public static void main(String[] arg) {
 		new QuestionScreen("QuestionScreen Test");
