43890fa0fdd57cbfe45fdd6ec22c723629c62b5d
Nikebeamz <allenyang31415@gmail.com>
2022-02-20 13:07:50 -0500

    File chooser added (needs testing)
    added file chooser
    
    for qbutton, removed quotes around text as constructor wasn't working properly

--- CCProject/src/ImportScreen.java
+++ CCProject/src/ImportScreen.java
@@ -9,23 +9,29 @@ class ImportScreen extends QPanel implements ActionListener{
   private TransitionButton back;
   private EstablisherButton chooseFile;
   private JLabel fileName = new JLabel("");
+  private JFileChooser jfc;
   ImportScreen(String title){
     super(title);
     int width = 80;
     int height = 40;
     
     back = new TransitionButton(width, height, Color.WHITE, "Back", 8, 0);
+    back.setBounds(20,20, width, height);
     back.setActionCommand("back");
     back.addActionListener(this);
     
     done = new TransitionButton(width, height, Color.WHITE, "Done", 8, 1);
+    done.setBounds(600,600, width, height);
     done.setActionCommand("done");
     done.addActionListener(this);
     done.setVisible(false);
     
     chooseFile = new EstablisherButton(width, height, Color.WHITE, "Choose File", 8, 2);
+    chooseFile.setBounds(480,560, width, height);
     chooseFile.setActionCommand("choose");
     chooseFile.addActionListener(this);
+    
+    fileName.setBounds(600, 560, 100, 40);
   }
   public int getScreenID(){
     return 8; 
@@ -42,7 +48,20 @@ class ImportScreen extends QPanel implements ActionListener{
         break;
         //send to main menu
       case 2:
-    	  break;
+    	  jfc = new JFileChooser();
+
+  		int returnValue = jfc.showOpenDialog(null);
+  		// int returnValue = jfc.showSaveDialog(null);
+
+  		if (returnValue == JFileChooser.APPROVE_OPTION) {
+  			File file = jfc.getSelectedFile();
+  			if(accept(file)) {
+  				done.setVisible(true);
+  				fileName = new JLabel(file.toString());
+  			}
+  			
+  		}
+    	break;
     }      
   }
   private boolean accept(File f){
@@ -51,11 +70,12 @@ class ImportScreen extends QPanel implements ActionListener{
       int index = name.lastIndexOf('.');
       if(index > 0) {
         String extension = name.substring(index + 1);
-        if(extension.equals("xml")) 
+        if(extension.equals("xml")) {
         	return true;
+        }
       }
       return false;
-    }
+  }
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		int id = 0;
