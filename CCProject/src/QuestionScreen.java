588c55c5f088672be86927debf6ec76892cac379
kchen3490 <kchen3490@gmail.com>
2022-02-20 22:15:00 -0500

    Another Test

--- CCProject/src/QuestionScreen.java
+++ CCProject/src/QuestionScreen.java
@@ -8,12 +8,13 @@ public class QuestionScreen extends QPanel implements ActionListener {
 	private String title;
 	private int id;
 	private boolean graphicDetected;
+	private JLabel titleLabel;
 	private JTextField questionBox;
 	private JTextField answerBox;
 	
-	private establisherBtn attachGraphic;
-	private establisherBtn detachGraphic;
-	private transitionBtn backBtn;
+	private EstablisherBtn attachGraphic;
+	private EstablisherBtn detachGraphic;
+	private TransitionBtn backBtn;
 
 	QuestionScreen(String t) {
 		super(t);//Will this set the title variable in this class to t?
@@ -21,6 +22,12 @@ public class QuestionScreen extends QPanel implements ActionListener {
 		    id = 12;
 		else
 		    id = 13;
+		titleLabel = new JLabel(title);
+		titleLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		titleLabel.setAlignmentY(JLabel.TOP_ALIGNMENT);
+		attachGraphic = new EstablisherBtn(161,44,Color.BLACK,"Attach",0);
+		detachGraphic = new EstablisherBtn(161,44,Color.BLACK,"Detach",1);
+		backBtn = new TransitionBtn(175,67,Color.BLACK,"Back",2);
 	}
 	public int getScreenID() {
 		return id;
@@ -51,8 +58,9 @@ public class QuestionScreen extends QPanel implements ActionListener {
 	private void radioClick() {
 		
 	}
-	public void main paintComponent(Graphic g) {
-		g.drawRectangle(1280,720,
+	public void paintComponent(Graphics g) {
+		g.setColor(BACKGROUND_COLOR);
+		g.drawRectangle(0,0,1280,720);
 	}
 	public static void main(String[] arg) {
 		new QuestionScreen("QuestionScreen Test");
