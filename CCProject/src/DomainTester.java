d5caec00e8b0c8c370fe6e859a6c3058c1aab21b
NathanDayie <98541724+NathanDayie@users.noreply.github.com>
2022-02-22 12:06:06 -0500

    Edit Domain and added xml file

---
+++ CCProject/src/DomainTester.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+
+public class DomainTester {
+	public static void main(String args[]) {
+		Question q1= new Question("1+1?", "2");
+		Question q2= new Question("1+2?", "3");
+		Question q3= new Question("1+3?", "4");
+		Question q4= new Question("1+4?", "5");
+		Question q5= new Question("1+5?", "6");
+		Question q6= new Question("1+6?", "7");
+		Question q7= new Question("1+7?", "8");
+		Question q8= new Question("1+8?", "9");
+		Question q9= new Question("1+9?", "10");
+		Question q10= new Question("1+10?", "11");
+		ArrayList<Question> questions = new ArrayList<Question>();
+		questions.add(q1);
+		questions.add(q2);
+		questions.add(q3);
+		questions.add(q4);
+		questions.add(q5);
+		questions.add(q6);
+		questions.add(q7);
+		questions.add(q8);
+		questions.add(q9);
+		questions.add(q10);
+		Domain n = new Domain("Simple Math", questions);
+		for(int i=0; i<n.getDomainSize(); i++) {
+			System.out.println(n.getQuestion(i).getQuestion());
+			System.out.println(n.getQuestion(i).getAnswer()+"\n");
+		}
+		System.out.println(n.export().toString());
+		
+	}
+}
---
+++ CCProject/src/QuestionCard.java
@@ -0,0 +1,11 @@
+import java.awt.image.*;
+
+public class QuestionCard {
+	String question;
+	String answer;
+	EstablisherButton ansBtn;
+	EstablisherButton knewAnsBtn;
+	EstablisherButton notKnewAnsBtn;
+	EstablisherButton nextQuesBtn;
+	BufferedImage questImage;
+}
