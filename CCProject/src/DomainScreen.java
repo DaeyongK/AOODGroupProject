fb5de41976321b9b8c502791ca84a7e2999ba3a7
PinkishUnicorns <98037140+PinkishUnicorns@users.noreply.github.com>
2022-02-22 11:01:42 -0500

    Update DomainScreen.java

--- CCProject/src/DomainScreen.java
+++ CCProject/src/DomainScreen.java
@@ -1,8 +1,27 @@
 import java.awt.*;
-
+import java.util.*;
+import javax.swing.*;
 public abstract class DomainScreen extends QPanel{
-	DomainScreen(String input){
+	protected ArrayList<EstablisherButton> domainButtons;
+	protected TransitionButton exit;
+	DomainScreen(String input, Profile profile){
 		super(input);
+		JPanel pane = new JPanel();
+		pane.setLayout(new BoxLayout(pane, BoxLayout.PAGE_AXIS));
+
+		ArrayList<Domain> domains = profile.getDomains();
+		for(int i = 0; i< domains.size();i++) {
+			domainButtons.add(new EstablisherButton(800, 50,Color.white,domains.get(i).getDomainName(),i));
+		}
+	
+		for(EstablisherButton button : domainButtons) {
+			pane.add(button);
+			button.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		}
+		JScrollPane scroll = new JScrollPane(pane);
+		add(scroll);
+		add(exit);
+
 	}
 	public String getName() {
 		return title; 
