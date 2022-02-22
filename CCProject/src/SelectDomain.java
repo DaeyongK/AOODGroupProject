23605478697c08efcf036e2d900028db71a904a2
PinkishUnicorns <98037140+PinkishUnicorns@users.noreply.github.com>
2022-02-22 10:18:59 -0500

    Update SelectDomain.java

--- CCProject/src/SelectDomain.java
+++ CCProject/src/SelectDomain.java
@@ -1,4 +1,27 @@
 
 public class SelectDomain extends DomainScreen{
 
+	SelectDomain(String input) {
+		super(input);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void buttonClicked(int buttonID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getScreenID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean popup(String text) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
