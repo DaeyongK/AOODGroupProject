d6f413181fc731c7774c0c1c3240c3fd3c283f25
Aben2901 <abner04ben@gmail.com>
2022-02-22 12:07:04 -0500

    buttons

--- CCProject/src/EstablisherButton.java
+++ CCProject/src/EstablisherButton.java
@@ -1,18 +1,32 @@
 import javax.swing.*;
+import javax.swing.event.MouseInputAdapter;
+
+import org.w3c.dom.events.MouseEvent;
+
 import java.awt.*;
 //Abner Ben
 public class EstablisherButton extends QButton{
-	EstablisherButton(int width, int height, Color color, String text, int screenID, int buttonID){
+	private int buttonId;
+	EstablisherButton(int width, int height, Color color, String text, int buttonID){
 		super(width,height,color,text,buttonID);
-		JButton b=new JButton(text);  
+		JButton b = new JButton(text);  
 		b.setBounds(50,50,width,height);
 		b.setBackground(TITLE_COLOR);
-		
+		buttonId=buttonID;
 	}
 	public void mousePressed() {
-		
+		addMouseMotionListener(new MouseInputAdapter() {
+			public void MousePressed(MouseEvent e) {
+				
+			}
+		});
 	}
 	public void mouseOver() {
+		addMouseMotionListener(new MouseInputAdapter() {
+			public void MouseEnter(MouseEvent e) {
+				
+			}
+		});
 	}
 
 }
--- CCProject/src/QButton.java
+++ CCProject/src/QButton.java
@@ -6,7 +6,7 @@ abstract class QButton extends JButton{
 	final Color TITLE_COLOR= new Color(240,193,67);//yellow
 	private int buttonId;
 	QButton(int width, int height, Color color, String text, int buttonID){
-		JButton b=new JButton(text);  
+		JButton b = new JButton(text);  
 		b.setBounds(50,50,width,height);
 		b.setBackground(TITLE_COLOR);
 		buttonId = buttonID;
