d1270d29946a38981200b695d785e2f32036ec5f
DaeyongK <>
2022-02-23 14:25:11 -0500

    Prototyped Profile Class

--- CCProject/src/Profile.java
+++ CCProject/src/Profile.java
@@ -1,17 +1,45 @@
 import java.util.*;
 import java.io.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 public class Profile {
     private String profileName;
     private int threshold, image;
     private boolean order = false;
     private boolean allQuestions = true;
-    private HashMap<Integer, int[]> questions = new HashMap<Integer, int[]>();
+    private LinkedHashMap<Integer, int[]> questions = new LinkedHashMap<Integer, int[]>();
     private ArrayList<Domain> domains = new ArrayList<Domain>();
     Profile(String profileName, int imageIndex) {
         this.profileName = profileName;
         image = imageIndex;
     }
-
+    Profile(File xml) {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(xml);
+            doc.getDocumentElement().normalize();
+            Node profile = doc.getElementsByTagName("profile").item(0);
+            Element pf = (Element) profile;
+            NodeList qs = doc.getElementsByTagName("question");
+            for(int i = 0; i < qs.getLength(); i++) {
+                Node question = qs.item(i);
+                Element q = (Element) question;
+                questions.put(Integer.parseInt(q.getElementsByTagName("id").item(0).getTextContent()),
+                new int[] {Integer.parseInt(q.getElementsByTagName("numRight").item(0).getTextContent()),
+                Integer.parseInt(q.getElementsByTagName("numAsked").item(0).getTextContent())});
+            }
+            NodeList ds = doc.getElementsByTagName("domainPath");
+            for(int i = 0; i < ds.getLength(); i++) {
+                String domain = ds.item(i).getTextContent();
+                domains.add(new Domain(new File(domain)));
+            }
+        } catch (Exception e) {}
+    }
     public String getName() {
         return profileName;
     }
