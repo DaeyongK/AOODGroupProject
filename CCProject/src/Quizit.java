14f2bf68f07cab3867ab54642b432eec15b46afb
DaeyongK <>
2022-02-24 11:00:20 -0500

    Added all basic methods

--- CCProject/src/Quizit.java
+++ CCProject/src/Quizit.java
@@ -1,26 +1,62 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 public class Quizit {
     private Profile currentProfile;
     private Question currentQuestion;
     private Domain currentDomain;
-    private Profile[] allProfiles;
-    
+    private ArrayList<Profile> profiles = new ArrayList<Profile>();
+    private MainMenu screen1;
+    private SelectProfile screen2;
+    private CreateProfile screen3;
+    private Options screen4;
+    private SelectDomains screen5;
+    private QuestionCard screen6;
+    private ImportScreen screen7;
+    private EditDomain screen8;
+    private ExportDomain screen9;
+    private DetachDomain screen10;
+    private QuestionScreen screen11, screen12;
     Quizit() {
-        MainMenu screen1 = new MainMenu("Main Menu");
-        SelectProfile screen2 = new SelectProfile("Select Profile");
-        CreateProfile screen3 = new CreateProfile("Create Profile");
-        Options screen4 = new Options("Quizzing Options");
-        SelectDomain screen5 = new SelectDomain("Select Domain");
-        QuestionCard screen6 = new QuestionCard("Question Card");
-        ImportScreen screen7 = new ImportScreen("Import Domain");
-        EditDomain screen8 = new EditDomain("Edit Domain");
-        ExportDomain screen9 = new ExportDomain("Export Domain");
-        DetachDomain screen10 = new DetachDomain("Detach Domain");
-        CreateQuestion screen11 = new CreateQuestion("Create Question");
-        EditQuestion screen12 = new EditQuestion("Edit Question");
+        screen1 = new MainMenu("", this);
+        screen2 = new SelectProfile("Select Profile", this);
+        screen3 = new CreateProfile("Create Profile", this);
+        screen4 = new Options("Quizzing Options", this);
+        screen5 = new SelectDomain("Select Domain", this);
+        screen6 = new QuestionCard("", this);
+        screen7 = new ImportScreen("Import Domain", this);
+        screen8 = new EditDomain("Edit Domain", this);
+        screen9 = new ExportDomain("Export Domain", this);
+        screen10 = new DetachDomain("Detach Domain", this);
+        screen11 = new QuestionScreen("Create Question");
+        screen12 = new QuestionScreen("Edit Question", this);
         frame = new JFrame("Quizit");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
+    public Profile getProfile() {
+        return currentProfile;
+    }
+    public void setProfile(Profile profile) {
+        currentProfile = profile;
+    }
+    public ArrayList<Profile> getAllProfiles() {
+        return profiles;
+    }
+    public void addProfile(Profile profile) {
+        profiles.add(profile);
+    }
+    public Question getQuestion() {
+        return currentQuestion;
+    }
+    public void setQuestion(Question question) {
+        currentQuestion = question;
+    }
+    public Domain getDomain() {
+        return currentDomain;
+    }
+    public void setDomain(Domain domain) {
+        currentDomain = domain;
+    }
+
 }
