5a89232931a3fbfbf863f1e54720d637a7945cd9
Aben2901 <abner04ben@gmail.com>
2022-02-24 11:15:01 -0500

    buttons

--- CCProject/src/EstablisherButton.java
+++ CCProject/src/EstablisherButton.java
@@ -1,29 +1,48 @@
 import javax.swing.*;
+
 import javax.swing.event.MouseInputAdapter;
 
 import org.w3c.dom.events.MouseEvent;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.*;
+
+import java.awt.event.ActionListener;
 //Abner Ben
 public class EstablisherButton extends QButton{
+	private QPanel panel;
+	private Color Color;
 	private int buttonId;
+	private int Width;
+	private int Height;
+	private String Text;
 	EstablisherButton(QPanel Panel,int width, int height, Color color, String text, int buttonID){
 		super(Panel,width,height,color,text,buttonID);
 		JButton b = new JButton(text);  
 		b.setBounds(50,50,width,height);
-		b.setBackground(TITLE_COLOR);
+		b.setBackground(color);
 		buttonId=buttonID;
+		Color=color;
+		panel=Panel;
+		Text=text;
+		Width=width;
+		Height=height;
+	}
+	public int getButtonID(){
+		return buttonId;
 	}
 	public void mousePressed() {
-		addMouseMotionListener(new MouseInputAdapter() {
-			public void MousePressed(MouseEvent e) {
-				
-			}
-		});
+		public void actionPerformed(ActionEvent event) {
+				EstablisherButton button = new EstablisherButton(panel,Width,Height,TITLE_COLOR,Text,buttonId);
+				panel.buttonClicked(buttonId);
+			
+		}
 	}
 	public void mouseOver() {
+		EstablisherButton button = new EstablisherButton(panel,Width,Height,Color,Text,buttonId);
 		addMouseMotionListener(new MouseInputAdapter() {
-			public void MouseEnter(MouseEvent e) {
+			public void MouseOver(MouseEvent e) {
 				
 			}
 		});
--- CCProject/src/TransitionButton.java
+++ CCProject/src/TransitionButton.java
@@ -6,24 +6,37 @@ import org.w3c.dom.events.MouseEvent;
 import java.awt.*;
 //Abner Ben
 public class TransitionButton extends QButton{
+	private QPanel panel;
+	private Color Color;
+	private int buttonId;
+	private int Width;
+	private int Height;
+	private String Text;
 	private int screenId;
 	TransitionButton(QPanel Panel,int width, int height, Color color, String text, int screenID, int buttonID){
 		super(Panel,width,height,color,text,buttonID);
 		JButton b = new JButton(text);  
 		b.setBounds(50,50,width,height);
-		b.setBackground(TITLE_COLOR);
+		b.setBackground(color);
 		screenId = screenID;
+		buttonId=buttonID;
+		Color=color;
+		panel=Panel;
+		Text=text;
+		Width=width;
+		Height=height;
 	}
 	public void mousePressed() {
 		addMouseMotionListener(new MouseInputAdapter() {
 			public void MousePressed(MouseEvent e) {
-				
+				EstablisherButton button = new EstablisherButton(panel,Width,Height,TITLE_COLOR,Text,buttonId);
+				panel.buttonClicked(buttonId);
 			}
 		});
 	}
 	public void mouseOver() {
 		addMouseMotionListener(new MouseInputAdapter() {
-			public void MousePressed(MouseEvent e) {
+			public void MouseOver(MouseEvent e) {
 				
 			}
 		});
