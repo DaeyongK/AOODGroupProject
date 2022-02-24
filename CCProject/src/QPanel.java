2cadc7dac129e7a30a084dc5edcc02abc852f034
pfulle8043 <98132740+pfulle8043@users.noreply.github.com>
2022-02-23 19:34:19 -0500

    Update QPanel.java
    made title color static

--- CCProject/src/QPanel.java
+++ CCProject/src/QPanel.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 
 public abstract class QPanel extends JPanel {
 	public final Color BACKGROUND_COLOR = new Color(32,18,77);
-	public final Color TITLE_COLOR = new Color(240,193,67);
+	public static final Color TITLE_COLOR = new Color(240,193,67);
 	public final Font font = new Font(Font.SANS_SERIF,Font.PLAIN,20);
 	protected String title;
 	protected int id = 0;
