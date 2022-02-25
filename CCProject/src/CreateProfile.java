81c35fae0b38a9ea274ad1fa2fca4b5fb703543e
toh8473 <terraoh8473@gmail.com>
2022-02-24 19:56:48 -0500

    Update CreateProfile.java

--- CCProject/src/CreateProfile.java
+++ CCProject/src/CreateProfile.java
@@ -3,6 +3,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -39,7 +40,8 @@ public class CreateProfile extends QPanel implements ActionListener{
 		create.addActionListener(this);
 		panel.add(create);
 		//shrek pfp
-		shrek = new CircularButton(this, 50, 50, Color.WHITE, title, 2, null);
+		BufferedImage shrekPFP = new BufferedImage("shrek.png");
+		shrek = new CircularButton(this, 50, 50, Color.WHITE, title, 2, );
 		shrek.addActionListener(this);
 		panel.add(shrek);
 		//fiona pfp
