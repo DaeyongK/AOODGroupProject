27242ce18ac46967bf1973d8ec938aeb5e438e29
LuqDude <luqmanforge@gmail.com>
2022-03-02 10:52:18 -0500

    fixed file paths

--- CCProject/src/CreateProfile.java
+++ CCProject/src/CreateProfile.java
@@ -52,7 +52,7 @@ public class CreateProfile extends QPanel implements MouseListener{
 		panel.add(textField, c);
 		
 		//shrek pfp
-		File shrekFile = new File("shrek.jpeg");
+		File shrekFile = new File("ProfilePictures/shrek.png");
 		Image shrekImage = null;
 		try {
 			shrekImage = ImageIO.read(shrekFile);
@@ -66,7 +66,7 @@ public class CreateProfile extends QPanel implements MouseListener{
 		panel.add(shrek, c);
 		
 		//fiona pfp
-		File fionaFile = new File("fiona.jpeg");
+		File fionaFile = new File("ProfilePictures/fiona.jpeg");
 		Image fionaImage = null;
 		try {
 			fionaImage = ImageIO.read(fionaFile);
@@ -80,7 +80,7 @@ public class CreateProfile extends QPanel implements MouseListener{
 		panel.add(fiona, c);
 		
 		//donkey pfp
-		File donkeyFile = new File("donkey.jpeg");
+		File donkeyFile = new File("ProfilePictures/donkey.jpeg");
 		Image donkeyImage = null;
 		try {
 			donkeyImage = ImageIO.read(donkeyFile);
