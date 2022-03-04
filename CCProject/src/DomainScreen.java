4ab3ee3ebfb8fa8cacfde539bf6883fcbbfac51d
PinkishUnicorns <98037140+PinkishUnicorns@users.noreply.github.com>
2022-03-04 12:04:54 -0500

    Update DomainScreen.java

--- CCProject/src/DomainScreen.java
+++ CCProject/src/DomainScreen.java
@@ -27,16 +27,21 @@ public abstract class DomainScreen extends QPanel {
         JScrollPane scroll = new JScrollPane(pane);
 //        scroll.setSize(500, 500);
         add(scroll);
-        scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);  
         scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);  
         if (domainButtons != null) {
-            for (EstablisherButton button : domainButtons) {
-        		pane.setLayout(new BoxLayout(pane, BoxLayout.PAGE_AXIS));
+            for (int i =0; i<domainButtons.size(); i++) {
+//        		pane.setLayout(new BoxLayout(pane, BoxLayout.PAGE_AXIS));
+        		pane.setLayout(new GridLayout(domainButtons.size(), 1));
+            	EstablisherButton button = domainButtons.get(i);
 
+        		button.setPreferredSize(new Dimension(800, 50));
             	pane.add(button);
+            	//pane.setLayout(null);
+//        		button.setBounds(20,80*i+20,360,50);
+//            	pane.add(button);
             }
         }
-        scroll.setBounds(400,150,400,400);
+        scroll.setBounds(150,150,900,400);
         exit = new TransitionButton(this, 100, 50, TITLE_COLOR, "Exit", 1, -1);
         exit.setBounds(50,50,50,50);
         titleLabel.setBounds(600,50,100,100);
