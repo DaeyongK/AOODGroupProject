c3ce5746454fda2e1afa4604a09ca187bcd94eb0
PinkishUnicorns <98037140+PinkishUnicorns@users.noreply.github.com>
2022-03-04 10:58:20 -0500

    AAAAAA

--- CCProject/src/DomainScreen.java
+++ CCProject/src/DomainScreen.java
@@ -15,25 +15,31 @@ public abstract class DomainScreen extends QPanel {
 		 */
         super(input, quizit);
 //		System.out.print(popup("yeah"));
-
-        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        setOpaque(true);
+        setLayout(null);
         domains = quizit.getProfile().getDomains();
         domainButtons = new ArrayList<>();
         for (int i = 0; i < domains.size(); i++) {
             domainButtons.add(new EstablisherButton(this, 800, 50, Color.white, domains.get(i).getDomainName(), i));
         }
+       
+        JPanel pane = new JPanel();
+        JScrollPane scroll = new JScrollPane(pane);
+//        scroll.setSize(500, 500);
+        add(scroll);
+        scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);  
+        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);  
         if (domainButtons != null) {
             for (EstablisherButton button : domainButtons) {
-                add(button);
-                button.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        		pane.setLayout(new BoxLayout(pane, BoxLayout.PAGE_AXIS));
+
+            	pane.add(button);
             }
         }
-
-        JScrollPane scroll = new JScrollPane(this);
-        scroll.setSize(500, 500);
+        scroll.setBounds(400,150,400,400);
         exit = new TransitionButton(this, 100, 50, TITLE_COLOR, "Exit", 1, -1);
-
-        exit.setBounds(50, 50, 50, 50);
+        exit.setBounds(50,50,50,50);
+        titleLabel.setBounds(600,50,100,100);
         add(exit);
 
     }
@@ -56,7 +62,7 @@ public abstract class DomainScreen extends QPanel {
         Testit t = new Testit();
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        DomainScreenTester test = new DomainScreenTester("Test Domain", t);
+        SelectDomain test = new SelectDomain("SelectDomain", t);
         frame.setContentPane(test);
         frame.pack();
         frame.setSize(1280, 720);
