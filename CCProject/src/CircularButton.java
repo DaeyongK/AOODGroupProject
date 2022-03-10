6b2bb06e233942cfb013724b35673b38e6b96b61
Aben2901 <abner04ben@gmail.com>
2022-03-10 10:59:33 -0500

    circular button

--- CCProject/src/CircularButton.java
+++ CCProject/src/CircularButton.java
@@ -6,12 +6,32 @@ import org.w3c.dom.events.MouseEvent;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-
+import java.awt.geom.Ellipse2D;
 public class CircularButton extends EstablisherButton {
+	private BufferedImage bim;
     CircularButton(QPanel Panel, int width, int height, Color color, String text, int buttonID, BufferedImage img) {
         super(Panel, width, height, color, text, buttonID);
-        setBorder(new RoundedBorder(100)); //how big to make it?
-        setIcon(new ImageIcon(img));
+        setButtonImage(img);
+        setBorder(new RoundedBorder(width)); //how big to make it? 
+        Graphics2D g2 = img.createGraphics();
+        g2.setClip(new Ellipse2D.Float(20, 400, width, width));
+        g2.drawImage(img, 20, 400, width, width, null);
+        Image newimg = img.getScaledInstance( width, height,  java.awt.Image.SCALE_SMOOTH );
+        setContentAreaFilled(false);
+        setIcon(new ImageIcon(newimg));
+    }
+    protected void paintComponent(Graphics g) {
+        g.setClip(new Ellipse2D.Double(0, 0, getWidth(), getHeight()));  // set the area that shall be painted
+        g.drawImage(bim, 0, 0, getWidth(), getHeight(), null);    // draw the image, if available
+        if (getModel().isArmed()) {      // show a slight gray shading when pressing the button
+            g.setColor(new Color(127, 127, 127, 80));   // gray with 80 as alpha
+            g.fillOval(0, 0, getSize().width - 1, getSize().height - 1);
+        }
+        super.paintComponent(g);
+    }
+    public void setButtonImage(BufferedImage pbim) {
+        bim = pbim;
+        repaint();
     }
     private static class RoundedBorder implements Border {
         private int radius;
