840edf8ef5a1b4fec95fcfa5bb7165f00497399b
toh8473 <terraoh8473@gmail.com>
2022-03-10 20:13:15 -0500

    asdfasdfas

--- CCProject/src/CreateProfile.java
+++ CCProject/src/CreateProfile.java
@@ -22,86 +22,19 @@ public class CreateProfile extends QPanel implements ActionListener {
     public CreateProfile(String title, Quizit q) {
         super(title, q);
         this.setLayout(null);
-<<<<<<< Updated upstream
-
-        //panel setup
-        this.setBackground(QPanel.TITLE_COLOR);
-        this.addMouseListener(this);
-        GridBagConstraints c = new GridBagConstraints();
-=======
         //panel setup
         this.setBackground(this.BACKGROUND_COLOR);
->>>>>>> Stashed changes
 
-        //create buttons and ui aspects. adds action listeners. then adds to jpanel.
+        //create buttons and ui aspects. adds action listeners. then adds to qpanel
         //exit button
         exit = new TransitionButton(this, 50, 25, TITLE_COLOR, "Exit", -1, 5);
-<<<<<<< Updated upstream
-        c.anchor = GridBagConstraints.FIRST_LINE_START;
-        exit.setBounds(0,50,50,50);
-        this.add(exit, c);
-=======
         exit.setBounds(30, 30, 100, 30);
         exit.addActionListener(this);
         this.add(exit);
->>>>>>> Stashed changes
 
         //text field
         textField = new JTextField();
         textField.setSize(70, 5);
-<<<<<<< Updated upstream
-        c.anchor = GridBagConstraints.PAGE_START;
-        this.add(textField, c);
-
-        //shrek pfp
-        File shrekFile = new File("ProfilePictures/shrek.png");
-        BufferedImage shrekImage = null;
-        try {
-            shrekImage = ImageIO.read(shrekFile);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        BufferedImage shrekPFP = shrekImage;
-        shrek = new CircularButton(this, 100, 100, Color.WHITE, title, 2, shrekPFP);
-        shrek.setBounds(200,200,100,100);
-        c.anchor = GridBagConstraints.LINE_START;
-        this.add(shrek, c);
-
-        //fiona pfp
-        File fionaFile = new File("ProfilePictures/fiona.jpeg");
-        BufferedImage fionaImage = null;
-        try {
-            fionaImage = ImageIO.read(fionaFile);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        BufferedImage fionaPFP = fionaImage;
-        fiona = new CircularButton(this, 50, 50, Color.WHITE, title, 3, fionaPFP);
-        c.anchor = GridBagConstraints.CENTER;
-        this.add(fiona, c);
-
-        //donkey pfp
-        File donkeyFile = new File("ProfilePictures/donkey.jpeg");
-        BufferedImage donkeyImage = null;
-        try {
-            donkeyImage = ImageIO.read(donkeyFile);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        
-        BufferedImage donkeyPFP = donkeyImage;
-        donkey = new CircularButton(this, 50, 50, Color.WHITE, title, 4, donkeyPFP);
-        c.anchor = GridBagConstraints.LINE_END;
-        this.add(donkey, c);
-
-        //create button
-        create = new TransitionButton(this, 50, 25, Color.WHITE, "Create", 4, 1);
-        c.anchor = GridBagConstraints.PAGE_END;
-        this.add(create, c);
-=======
         textField.setBounds(500, 200, 250, 50);
         textField.addActionListener(this);
         this.add(textField);
@@ -154,7 +87,6 @@ public class CreateProfile extends QPanel implements ActionListener {
         create.setBounds(560, 600, 100, 30);
         create.addActionListener(this);
         this.add(create);
->>>>>>> Stashed changes
     }
 
     public void radioClick() {
